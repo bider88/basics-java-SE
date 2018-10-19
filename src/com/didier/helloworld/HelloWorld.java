09358ad9b3a09ff11d26da05d1e2de7caab3e051
Irving Didier <bider88@gmail.com>
2018-10-18 22:51:23 -0500

    Initial commit

---
+++ src/com/didier/helloworld/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.didier.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		// Integer
+		byte age = 127;
+		short year = -32768;
+		int idUser = 1131234648;
+		long id_user = 2100129012992312411L;
+		
+		// Floats
+		float diameter = 34.25F;
+		double price = 2393353423.720232423423;
+		
+		// Text
+		char gender = 'F';
+		
+		// Boolean
+		boolean isVisible = true;
+		
+		// Constants
+		byte VALUE = 3;
+		
+		// Arrays
+		int [] arrayInt = new int[4];
+		int arrayInt2D [][] = new int[3][5];
+		int arrayInt3D [][][] = new int[3][3][2];
+		
+		System.out.println(VALUE);
+	}
+
+}
